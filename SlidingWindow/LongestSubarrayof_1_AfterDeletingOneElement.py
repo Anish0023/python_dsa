87376615a9d9973d3ebf6d1b5714e235c5586080
Anish005 <anishpanda31@gmail.com>
2025-08-24 23:10:28 +0530

    sliding window

--- SlidingWindow/LongestSubarrayof_1_AfterDeletingOneElement.py
+++ SlidingWindow/LongestSubarrayof_1_AfterDeletingOneElement.py
@@ -1,3 +1,7 @@
+'''Given a binary array nums, you should delete one element from it.
+longest subarray of atmost one zero
+
+'''
 class Solution(object):
     def longestSubarray(self, nums):
         """
@@ -17,3 +21,10 @@ class Solution(object):
                 l += 1
             max_len = max(max_len, r - l)
         return max_len
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.longestSubarray([1,1,0,1]))  # Output: 3
+    print(sol.longestSubarray([0,1,1,1,0,1,1,0,1]))  # Output: 5
+    print(sol.longestSubarray([1,1,1]))  # Output: 2
+    print(sol.longestSubarray([1,1,0,0,1,1,1,0,1]))  # Output: 4
+    print(sol.longestSubarray([0,0,0]))  # Output: 0
