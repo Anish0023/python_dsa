0e94e22aaca95585a5cfdb18eefd15d33c613841
Anish005 <anishpanda31@gmail.com>
2026-03-02 10:27:13 +0530

    rotate array

---
+++ arrays&matrix/rotatearray_left.py
@@ -0,0 +1,40 @@
+from typing import List
+'''
+BRUTE FORCE
+Time complexity: O(n*k) 
+def rotate(self,nums,k):
+    n = len(nums)
+    k%=n
+    for _ in range(k):
+        last = nums.pop(0)
+        nums.append(last)
+
+'''
+class Solution:
+    def reverse(self,nums,l ,r):
+        while l < r:
+            nums[l] ,nums[r] = nums[r] , nums[l]
+            l += 1
+            r -= 1
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k%=n
+        self.reverse(nums,0 ,k - 1)
+        self.reverse(nums,k, n - 1)
+        self.reverse(nums,0, n - 1)
+        print(nums)
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.rotate([1,2,3,4,5], 2))
+    print(sol.rotate([-1,-100,3,99], 3))
+
+'''
+class Solution:
+    def rotate(self, nums, k):
+        k = k % len(nums)          # Normalize k
+        nums[:] = nums[-k:] + nums[:-k]   # Slice & combine
+
+'''
