12b6c60c86dc30889ce085f0549412ac669317c6
Anish005 <anishpanda31@gmail.com>
2026-06-17 12:45:06 +0530

    DPP

--- inf_mocks/hk_es_2.py
+++ inf_mocks/hk_es_2.py
@@ -16,11 +16,49 @@ sys.stdout = open('/Users/aniiiish/Documents/Projects/python_dsa/inf_mocks/outpu
 sys.stdin = open('/Users/aniiiish/Documents/Projects/python_dsa/inf_mocks/input.txt','r')
 
 
-def solve():
-    pass
+from collections import defaultdict
+import heapq
+
+def maxGoodSubarraySum(A, k):
+    n = len(A)
+
+    prefix = [0] * (n + 1)
+    for i in range(n):
+        prefix[i + 1] = prefix[i] + A[i]
+
+    freq = defaultdict(int)
+    left = 0
+    ans = 0
+
+    # (prefix_sum, index)
+    heap = [(0, 0)]
+
+    for right in range(n):
+        freq[A[right]] += 1
+
+        while len(freq) > k:
+            freq[A[left]] -= 1
+            if freq[A[left]] == 0:
+                del freq[A[left]]
+            left += 1
+
+        # Remove prefixes that are no longer valid
+        while heap and heap[0][1] < left:
+            heapq.heappop(heap)
+
+        ans = max(ans, prefix[right + 1] - heap[0][0])
+
+        heapq.heappush(heap, (prefix[right + 1], right + 1))
+
+    return ans
 def main():
     t = int(input())
     for _ in range(t):
-        pass
+        n = int(input())
+        k = int(input())
+        A = []
+        for i in range(n):
+            A.append(int(input()))
+        print(maxGoodSubarraySum(A,k))
 if __name__ == '__main__':
     main()
---
+++ inf_mocks/hk_es_3.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdout = open('/Users/aniiiish/Documents/Projects/python_dsa/inf_mocks/output.txt', 'w')
+sys.stdin = open('/Users/aniiiish/Documents/Projects/python_dsa/inf_mocks/input.txt','r')
+
+def solve():
+    pass
+def main():
+    t = int(input())
+    for _ in range(t):
+        pass
+if __name__ == '__main__':
+    main()
